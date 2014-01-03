34a8ed0282ddff3065d685ccfb9f4c9446e23979
Marius Schmidt <marius@denktmit.de>
2014-01-03 13:50:15 +0100

    Removed demo stuff, added map handling

--- www/js/index.js
+++ www/js/index.js
@@ -18,25 +18,66 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
+
+        app.resizeApp();
+
+        var map = L.map('map-canvas').setView([50.111778, 8.70115], 14);
+
+        //this works, but is online:
+/*
+
+         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         maxZoom: 18
+         }).addTo(map);
+
+*/
+
+        //TODO build something to fall back to web if not found.
+
+        var southWest = new L.LatLng(50.02893, 8.525391);
+        var northEast = new L.LatLng(50.169862, 8.860452);
+        var restrictBounds = new L.LatLngBounds(southWest, northEast);
+
+        L.tileLayer('img/mapTiles/{z}/{x}/{y}.png', {
+            maxBounds: restrictBounds,
+            minZoom: 14,
+            maxZoom: 16
+        }).addTo(map);
+
+
+        L.marker([50.111778, 8.70115]).addTo(map)
+            .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+        var popup = L.popup();
+
+        function onMapClick(e) {
+            popup
+                .setLatLng(e.latlng)
+                .setContent("You clicked the map at " + e.latlng.toString())
+                .openOn(map);
+        }
+
+        map.on('click', onMapClick);
+
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -45,5 +86,53 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    resizeApp: function () {
+        $("#app").height($(window).height());
+        $("#app").width($(window).width());
+        var navHeight = $("#nav").outerHeight();
+        $("#map-canvas").height($(window).height() - navHeight);// TODO set
     }
 };
+
+$(window).resize(function () {
+    app.resizeApp();
+});
+
+/*
+ var LeafIcon = L.Icon.extend({
+ options: {
+ shadowUrl: 'leaf-shadow.png',
+ iconSize:     [38, 95],
+ shadowSize:   [50, 64],
+ iconAnchor:   [22, 94],
+ shadowAnchor: [4, 62],
+ popupAnchor:  [-3, -76]
+ }
+ });
+
+ var greenIcon = new LeafIcon({iconUrl: 'leaf-green.png'}),
+ redIcon = new LeafIcon({iconUrl: 'leaf-red.png'}),
+ orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
+
+ var appConfig = {
+ "tour": {
+ "name": "Passionsspiel in Frankfurt",
+ "desc": "Bilder mit Übersichtskarten oder alten Stadtteilansichten sind mit einem (?)-Symbol, interessante Orte mit einem (i)-Symbol markiert. Auch Linien und Flächensignaturen und andere Signaturen enthalten Informationen und Bilder.",
+ "pointsOfInterest": [
+ {"title": "", "desc": "", "iconType": greenIcon, }
+ {"title": "", "desc": "", "iconType": redIcon, }
+ {"title": "", "desc": "", "iconType": orangeIcon, }
+ {"title": "", "desc": "", "iconType": greenIcon, }
+ {"title": "", "desc": "", "iconType": redIcon, }
+ {"title": "", "desc": "", "iconType": orangeIcon, }
+ ]
+ }
+ "legalNotice": {
+ "authors": [
+ {"name": "Marius Schmidt", "email": "info@denktmit.de"}
+ ],
+ },
+ }
+ */
