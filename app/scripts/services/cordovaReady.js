b5600e6c85d98a77a7f48fd121278e69be8ea1c8
Phonegap <holland@ubuntu.(none)>
2014-01-28 10:44:15 -0800

    added first distance calculations.

---
+++ app/scripts/services/cordovaReady.js
@@ -0,0 +1,15 @@
+'use strict';
+
+SchreibwerkstattApp.factory('cordovaReady', function() {
+  // Service logic
+  // ...
+
+  var meaningOfLife = 42;
+
+  // Public API here
+  return {
+    someMethod: function() {
+      return meaningOfLife;
+    }
+  };
+});
---
+++ www/js/services.js
@@ -0,0 +1,51 @@
+var appServices = angular.module('appServices', []);
+
+appServices.factory('cordovaReady', function() {
+  return function (fn) {
+
+    var queue = [];
+
+    var impl = function () {
+      queue.push(Array.prototype.slice.call(arguments));
+    };
+
+    document.addEventListener('deviceready', function () {
+      queue.forEach(function (args) {
+        fn.apply(this, args);
+      });
+      impl = fn;
+    }, false);
+
+    return function () {
+      return impl.apply(this, arguments);
+    };
+  };
+});
+
+
+appServices.factory('geolocation', function ($rootScope, cordovaReady) {
+  return {
+    getCurrentPosition: cordovaReady(function (onSuccess, onError, options) {
+      navigator.geolocation.watchPosition(function () {
+        var that = this,
+          args = arguments;
+
+        if (onSuccess) {
+          $rootScope.$apply(function () {
+            onSuccess.apply(that, args);
+          });
+        }
+      }, function () {
+        var that = this,
+          args = arguments;
+
+        if (onError) {
+          $rootScope.$apply(function () {
+            onError.apply(that, args);
+          });
+        }
+      },
+      {enableHighAccuracy: true});
+    })
+  };
+});
