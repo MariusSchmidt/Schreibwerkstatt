5b884f4b90b1f641129f2c26c8f8272b0cb7a453
Phonegap <holland@ubuntu.(none)>
2014-02-28 06:50:38 -0800

    First unittesting steps - Added ControllerSpec and Karma config

---
+++ www/js/tests/controllersSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('controllers', function(){
+    beforeEach(module('appControllers'));
+
+    describe('PoiCtrl', function(){
+    var $scope, $rootScope;
+    it('should exist', inject(function(){
+        expect(true).toBe(true);
+        //spec body
+    }))
+    });
+})
+
+
+
+
