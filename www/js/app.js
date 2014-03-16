9e0283e4764c8e4204134c4d1555ae173b4098c8
Marius Schmidt <marius@denktmit.de>
2014-03-16 12:57:36 +0100

    working on image view / merge

--- www/js/app.js
+++ www/js/app.js
@@ -38,7 +38,7 @@ schreibwerkApp.config(['$routeProvider',
             }).
             when('/directive', {
                 templateUrl: 'partials/directive.html',
-                controller: 'PoiCtrl'
+                controller: 'MapCtrl'
             }).
             when('/imgview/:imgUrl', {
                 templateUrl: 'partials/imgview.html',
