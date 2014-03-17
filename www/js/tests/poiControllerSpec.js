dfe49f603130dd4b0d35e6ed5aa7c07bcd15da54
Darot <gikay182@yahoo.de>
2014-03-17 22:14:23 +0100

    added poi Controller tests

--- www/js/tests/poiControllerSpec.js
+++ www/js/tests/poiControllerSpec.js
@@ -2,37 +2,73 @@
 
 describe('PoiCtrl', function(){
     var $scope;
-    var routeParams
-    var geolocationMock
-    var mediaMock
-    var notificationMock
+    var routeParams;
+    var geolocationMock;
+    var mediaMock;
+    var notificationMock;
+    var locationMock;
+    var device;
+    var $location
 
     beforeEach(angular.mock.module('appControllers'));
 
     beforeEach(angular.mock.inject(function($rootScope, $controller){
         $scope = $rootScope.$new();
+
+        $location = {};
+        $location.path = function(path){
+            return path;
+        }
+
+        $scope.pois = [];
         routeParams = {};
+        routeParams.stationID = 1;
+        device = {};
+        device.width = 500;
 
         notificationMock = jasmine.createSpyObj('notification', ['confirm']);
         mediaMock = jasmine.createSpyObj('media', ['play', 'stop']);
         geolocationMock = jasmine.createSpyObj('geolocation', ['watchPosition', 'clearWatch']);
+        //locationMock = jasmine.createSpyObj('location', ['path']);
 
         //declaration of the controller and mock injection
         $controller('PoiCtrl', {
             $routeParams: routeParams,
             $scope: $scope,
             $rootScope: $rootScope,
+            $location: $location,
             geolocation: geolocationMock,
             media: mediaMock,
-            notification: notificationMock
+            notification: notificationMock,
+            device: device
         });
     }));
 
 
     it('should have some scope variables', function(){
+        expect($scope.stationID).toBe(1);
+        expect($scope.poi).toBe($scope.pois[1]);
+    });
 
-        $routeParams.stationID = 1;
-        expect(true).toBeTruthy();
+    it('should return a Style Object or nothing', function(){
+        device.width = 500;
+        expect($scope.getMargin().margin).toBe('50px');
+    });
+
+    it('should return width = 460(tablets) or width = 230(smartphones)', function(){
+        device.width = 500;
+        expect($scope.getImageWidth().width).toBe('460px');
+    });
+
+    it('should set redirect and stop the running Media', function(){
+        $location.path("/path");
+        $scope.stopAndRedirect("/path");
+        expect(show).toBeFalsy();
+        //expect($location.path("test")).toHaveBeenCalled();
+    })
+
+    it('should call media.play service', function(){
+
     })
 
 //    it('creates spies for each requested function', function(){
