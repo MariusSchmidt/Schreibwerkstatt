b756b62557fc6f1313540986f67acb1b7a76358a
Marius Schmidt <marius@denktmit.de>
2014-03-17 22:16:53 +0100

    Added $location dependency to directive and removed imagemap

--- www/js/directives.js
+++ www/js/directives.js
@@ -2,7 +2,7 @@
 
 var appDirectives = angular.module('appDirectives', ['phonegapServices']);
 
-appDirectives.directive('angularmap', function (Map, device) {
+appDirectives.directive('angularmap', function ($location, Map, device) {
 
     function link(scope, element, attrs) {
 
@@ -20,34 +20,30 @@ appDirectives.directive('angularmap', function (Map, device) {
 
         scope.mapClicked = function (event) {
 
-            this.clickX = event.clientX || event.changedTouches[0].clientX;
-            this.clickY = event.clientY || event.changedTouches[0].clientY;
+            var eventX = event.clientX || event.changedTouches[0].clientX;
+            var eventY = event.clientY || event.changedTouches[0].clientY;
 
-            this.wrapper = angular.element(element.children());
-            this.offsetLeft = this.wrapper.prop('offsetLeft');
-            this.offsetTop = this.wrapper.prop('offsetTop');
-            this.relativeX = this.clickX - this.offsetLeft;
-            this.relativeY = this.clickY - this.offsetTop;
-            this.shiftX = scope.canvas.width / 2.0 - this.relativeX;
-            this.shiftY = scope.canvas.height / 2.0 - this.relativeY;
+            var wrapper = angular.element(element.children());
+            var offsetLeft = wrapper.prop('offsetLeft');
+            var offsetTop = wrapper.prop('offsetTop');
 
+            var pixelCoords = {
+                left: eventX - offsetLeft - scope.mapOffset.left,
+                top: eventY - offsetTop - scope.mapOffset.top
+            }
+
+            this.goToPixelPosition(pixelCoords)
+
+
+
+        }
+
+        scope.goToPixelPosition = function(pixelCoords) {
             var newOffset = {
-                left: scope.mapOffset.left + this.shiftX,
-                top: scope.mapOffset.top + this.shiftY
+                left: scope.canvas.width / 2 - pixelCoords.left,
+                top: scope.canvas.height / 2 - pixelCoords.top
             }
             this.setMapOffset(newOffset);
-
-            console.log('offsetLeft: ' + this.offsetLeft);
-            console.log('offsetTop: ' + this.offsetTop);
-            console.log('clickX: ' + this.clickX);
-            console.log('clickY: ' + this.clickY);
-            console.log('relativeX: ' + this.relativeX);
-            console.log('relativeY: ' + this.relativeY);
-            console.log('shiftX: ' + this.shiftX);
-            console.log('shiftY: ' + this.shiftY);
-            console.log('map.offset.left: ' + scope.mapOffset.left);
-            console.log('map.offset.top: ' + scope.mapOffset.top);
-
         }
 
         scope.setMapOffset = function(offset) {
@@ -73,11 +69,8 @@ appDirectives.directive('angularmap', function (Map, device) {
                 return;
             }
             var pixelCoords = Map.geoToPixels(scope.userPosition);
-            var newOffset = {
-                left: scope.canvas.width / 2 - pixelCoords.left,
-                top: scope.canvas.height / 2 - pixelCoords.top
-            }
-            this.setMapOffset(newOffset);
+            scope.goToPixelPosition(pixelCoords);
+
         }
 
         scope.containerStyle = function () {
