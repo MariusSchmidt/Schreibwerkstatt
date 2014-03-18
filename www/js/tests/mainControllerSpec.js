ef1b40ff18e82f2563c5def73b3999d7ebff76e0
Darot <gikay182@yahoo.de>
2014-03-18 12:22:25 +0100

    Fully Main controller spec

--- www/js/tests/mainControllerSpec.js
+++ www/js/tests/mainControllerSpec.js
@@ -3,19 +3,23 @@
 describe('MainCtrl',function(){
     var $scope;
     var geolocation;
+    var position;
 
     beforeEach(angular.mock.module('appControllers'));
 
     beforeEach(inject(function($rootScope, $controller){
         $scope = $rootScope.$new();
 
+        position = {};
+
+        position.coords = {
+            longitude: 1.0,
+            latitude: 1.0
+        }
+
         geolocation = {
             watchPosition: function(onSuccess){
-                var position = {
-                    latitude: 1.0,
-                    longitude: 1.0
-                }
-                onSuccess.apply(position)
+                onSuccess(position);
             }
         };
 
@@ -23,9 +27,21 @@ describe('MainCtrl',function(){
         $controller('MainCtrl', {
             $scope: $scope,
             geolocation: geolocation,
-            $routeParams: routeParams
+            position: position
         })
     }))
+
+    it('should have some variable defined in scope', function() {
+        expect($scope.mapOffset.top).toBe(0);
+        expect($scope.pois[0].title).toBe("Einführung");
+        expect($scope.poi.title).toBe("Einführung");
+    })
+
+    it('should watch position and save it in $scope.pos', function(){
+        expect($scope.pos.latitude).toBe(1.0);
+    })
+
+    
 
 
 })
